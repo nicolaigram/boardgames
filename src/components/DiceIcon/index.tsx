3091543f9eb9474398235df24df229f679ad7c87
Nicolai Gram <nicolaigram@gmail.com>
2020-09-03 19:46:41 +0200

    Add rules

---
+++ src/components/DiceIcon/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export default function DiceIcon({
+  name,
+  amount,
+  rule,
+}: {
+  name: string;
+  amount: number;
+  rule: string;
+}) {
+  const url = "/dice/" + name;
+  const size = 50;
+  return (
+    <RuleContainer>
+      {Array(amount)
+        .fill(0)
+        .map((_: any, index: any) => (
+          <img
+            src={url}
+            alt={name}
+            style={{ marginRight: "10px" }}
+            height={size}
+            width={size}
+          />
+        ))}
+      <p>{rule}</p>
+    </RuleContainer>
+  );
+}
+
+const RuleContainer = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  color: white;
+`;
---
+++ src/components/RulesPage/index.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import DiceIcon from "../DiceIcon";
+import styled from "@emotion/styled";
+
+export default function RulesPage() {
+  return (
+    <Background>
+      <Overlay>
+        <Flex>
+          <DiceIcon name="01_heart.png" amount={1} rule="Heal x" />
+          <DiceIcon name="01_heart.png" amount={3} rule="Take power-up card" />
+        </Flex>
+        <DiceIcon
+          name="02_destroy.png"
+          amount={1}
+          rule="Damage x to buildings or units"
+        />
+        <DiceIcon name="03_energy.png" amount={3} rule="Take x energy" />
+        <DiceIcon
+          name="04_skull.png"
+          amount={1}
+          rule="Units in your borough you damage you"
+        />
+        <DiceIcon
+          name="04_skull.png"
+          amount={2}
+          rule="+ other monsters in your borough"
+        />
+        <DiceIcon
+          name="04_skull.png"
+          amount={3}
+          rule="All units damage all monsters + take the Statue of Liberty"
+        />
+        <Flex>
+          <DiceIcon name="05_claw.png" amount={1} rule="Do x damage" />
+          <DiceIcon
+            name="05_claw.png"
+            amount={3}
+            rule="Berserk (if using expansion)"
+          />
+        </Flex>
+
+        <DiceIcon name="06_star.png" amount={1} rule="Nothing*" />
+        <DiceIcon name="06_star.png" amount={3} rule="Take Superstar card" />
+      </Overlay>
+    </Background>
+  );
+}
+
+const Background = styled.div`
+  background: url("/bg.png");
+  background-position: center;
+  background-size: cover;
+  height: 100vh;
+  padding: 50px;
+`;
+
+const Overlay = styled.div`
+  padding: 25px;
+  background: rgba(0, 0, 0, 0.8);
+`;
+
+const Flex = styled.div`
+  display: flex;
+  div {
+    margin-right: 10px;
+  }
+`;
