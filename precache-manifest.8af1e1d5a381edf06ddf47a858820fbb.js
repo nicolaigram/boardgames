03708956c9ac9d48bbb08985f02c97c0c14b2daf
Nicolai Gram <nicolaigram@gmail.com>
2020-09-03 19:49:59 +0200

    Updates

---
+++ precache-manifest.8af1e1d5a381edf06ddf47a858820fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c48cf9300a3096a748eab81b2da5b5",
+    "url": "/boardgames/index.html"
+  },
+  {
+    "revision": "4605f4cea583a0defe10",
+    "url": "/boardgames/static/css/main.8e18de48.chunk.css"
+  },
+  {
+    "revision": "4dc9c67f6b2495b683ad",
+    "url": "/boardgames/static/js/2.2a5c8fb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boardgames/static/js/2.2a5c8fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4605f4cea583a0defe10",
+    "url": "/boardgames/static/js/main.3a0845db.chunk.js"
+  },
+  {
+    "revision": "21870fb52d6d40d84448",
+    "url": "/boardgames/static/js/runtime-main.e4470165.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc9c8041afc4ea5c982b729b560a56c3.js"
+  "/boardgames/precache-manifest.8af1e1d5a381edf06ddf47a858820fbb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/boardgames/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
