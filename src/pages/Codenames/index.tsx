2555e54a909dbb2d8be15b8a74b8b61086d46ab1
Nicolai Gram <nicolaigram@gmail.com>
2020-09-28 14:52:56 +0200

    Codenames bg

--- src/pages/Codenames/index.tsx
+++ src/pages/Codenames/index.tsx
@@ -9,68 +9,83 @@ export default function Codenames() {
 
   return (
     <StyledPage>
-      <div className="col options">
-        <h2>Language</h2>
-        <div
-          className={"option" + (language === "da" ? " active" : "")}
-          onClick={() => setLanguage("da")}
-        >
-          Danish
+      <div className="columns">
+        <div className="col options">
+          <h2>Language</h2>
+          <div
+            className={"option" + (language === "da" ? " active" : "")}
+            onClick={() => setLanguage("da")}
+          >
+            Danish
+          </div>
+          <div
+            className={"option" + (language === "en" ? " active" : "")}
+            onClick={() => setLanguage("en")}
+          >
+            English
+          </div>
         </div>
-        <div
-          className={"option" + (language === "en" ? " active" : "")}
-          onClick={() => setLanguage("en")}
-        >
-          English
+        <div className="col options">
+          <h2>Version</h2>
+          <div
+            className={"option" + (version === "standard" ? " active" : "")}
+            onClick={() => setVersion("standard")}
+          >
+            Standard
+          </div>
+          <div
+            className={"option" + (version === "duet" ? " active" : "")}
+            onClick={() => setVersion("duet")}
+          >
+            Duet
+          </div>
         </div>
-      </div>
-      <div className="col options">
-        <h2>Version</h2>
-        <div
-          className={"option" + (version === "standard" ? " active" : "")}
-          onClick={() => setVersion("standard")}
-        >
-          Standard
+        <div className="col">
+          <h2>Game</h2>
+          <Link to={`/codenames/game/${version}/${language}/default`}>
+            Standard
+          </Link>
+          <Link to={`/codenames/game/${version}/${language}/deep`}>
+            Deep Undercover
+          </Link>
         </div>
-        <div
-          className={"option" + (version === "duet" ? " active" : "")}
-          onClick={() => setVersion("duet")}
-        >
-          Duet
+        <div className="col">
+          <h2>Tools</h2>
+          <Link to={`/codenames/keycard/${version}`}>KeyCard</Link>
         </div>
-      </div>
-      <div className="col">
-        <h2>Game</h2>
-        <Link to={`/codenames/game/${version}/${language}/default`}>
-          Standard
-        </Link>
-        <Link to={`/codenames/game/${version}/${language}/deep`}>
-          Deep Undercover
-        </Link>
-      </div>
-      <div className="col">
-        <h2>Tools</h2>
-        <Link to={`/codenames/keycard/${version}`}>KeyCard</Link>
       </div>
     </StyledPage>
   );
 }
 
 const StyledPage = styled.div`
-  display: flex;
-  justify-content: center;
-  padding-top: 20px;
-  flex-wrap: wrap;
+  padding-top: 40px;
+  background: center url(${process.env.PUBLIC_URL}/codenames/bg.jpeg);
+  background-size: cover;
+  min-height: 100vh;
   @media screen and (max-width: 750px) {
     width: 100%;
+    padding-right: 16px;
+    padding-left: 16px;
+  }
+  .columns {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    background: rgba(0, 0, 0, 0.6);
+    max-width: 800px;
+    margin: 0 auto;
   }
   .col {
-    margin-right: 20px;
+    padding: 0 10px;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
+    font-weight: 300;
     h2 {
       text-align: center;
+      color: white;
+      font-weight: 400;
     }
     a,
     .option {
@@ -84,13 +99,7 @@ const StyledPage = styled.div`
     }
   }
   .options {
-    background: rgba(0, 0, 0, 0.3);
-    padding: 0 10px;
     padding-right: 20px;
-    &:not(:nth-of-type(2)) {
-      padding-right: 10px;
-      margin-right: 0;
-    }
     .option {
       &.active {
         color: ${red};
